db12b181eeebb1be3333efea6cd5561101d569ff
tranbatong <tongxayda00@gmail.com>
2026-03-15 23:18:43 +0700

    feat: add base frontend project

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food hear</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet qui
+          repellat fugiat eius! Dolore error ullam cupiditate id, ad molestiae?
+        </p>
+        <button> View menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItem] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItem((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+  const removeFromCart = (itemId) => {
+    setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItem,
+    addToCart,
+    removeFromCart,
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
