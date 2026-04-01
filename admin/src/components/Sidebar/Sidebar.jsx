f2d62adcf78e73984231403240f733794680ca14
tranbatong <tongxayda00@gmail.com>
2026-04-01 12:21:27 +0700

    style: add ux ui admin

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,38 +1,62 @@
 import React from "react";
+import { Layout, Menu } from "antd";
+import { NavLink, useLocation } from "react-router-dom";
+import {
+  PlusCircleOutlined,
+  UnorderedListOutlined,
+  ShoppingOutlined,
+  MessageOutlined,
+  UserOutlined,
+  TagsOutlined,
+} from "@ant-design/icons";
 import "./Sidebar.css";
-import { assets } from "../../assets/assets";
-import { NavLink } from "react-router-dom";
+
+const { Sider } = Layout;
 
 const Sidebar = () => {
+  const location = useLocation();
+
+  // Danh sách các menu item để code gọn hơn
+  const menuItems = [
+    {
+      key: "/list",
+      icon: <UnorderedListOutlined />,
+      label: <NavLink to="/list">List Items</NavLink>,
+    },
+    {
+      key: "/orders",
+      icon: <ShoppingOutlined />,
+      label: <NavLink to="/orders">Orders</NavLink>,
+    },
+    {
+      key: "/chat",
+      icon: <MessageOutlined />,
+      label: <NavLink to="/chat">Messages</NavLink>,
+    },
+    {
+      key: "/users",
+      icon: <UserOutlined />,
+      label: <NavLink to="/users">Users</NavLink>,
+    },
+    {
+      key: "/vouchers",
+      icon: <TagsOutlined />,
+      label: <NavLink to="/vouchers">Vouchers</NavLink>,
+    },
+  ];
+
   return (
-    <div className="sidebar">
-      <div className="sidebar-options">
-        <NavLink to="/add" className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
-          <p>Add Items</p>
-        </NavLink>
-        <NavLink to="/list" className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>List Item</p>
-        </NavLink>
-        <NavLink to="/orders" className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>Order</p>
-        </NavLink>
-        <NavLink to="/chat" className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>Messages</p>
-        </NavLink>
-        <NavLink to="/users" className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>Users</p>
-        </NavLink>
-        <NavLink to="/vouchers" className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
-          <p>Vouchers</p>
-        </NavLink>
+    <Sider width={240} theme="light" className="custom-sidebar" breakpoint="lg">
+      <div className="sidebar-logo">
+        <h2 className="brand-text">ADMIN PANEL</h2>
       </div>
-    </div>
+      <Menu
+        mode="inline"
+        selectedKeys={[location.pathname]}
+        items={menuItems}
+        className="main-menu"
+      />
+    </Sider>
   );
 };
 
